4dec9f26cdbfa8da561c126a0d3fd320abe5b3b4
kien <dp1.1a2kien@gmail.com>
2024-09-08 22:39:55 +0700

    xây dựng phần danh sách nhóm quyền

---
+++ controller/admin/role.controller.js
@@ -0,0 +1,28 @@
+const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
+// [get] /admin/role
+
+module.exports.index = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+  const records = await Role.find(find);
+  res.render("admin/pages/roles/index", {
+    pageTitle: "Nhóm quyền",
+    records: records,
+  });
+};
+// [get] /admin/role/create
+
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/roles/create", {
+    pageTitle: "Tạo nhóm quyền",
+  });
+};
+// [post] /admin/role/createPost
+
+module.exports.createPost = async (req, res) => {
+  const record = new Role(req.body);
+  await record.save();
+  res.redirect(`${systemConfig.prefixAdmin}/roles`);
+};

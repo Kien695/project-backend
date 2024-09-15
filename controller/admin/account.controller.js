8c55a7fd13fc25224ea2a59d251d05dddd571ab7
kien <dp1.1a2kien@gmail.com>
2024-09-15 13:44:55 +0700

    trang sửa tài khoản

--- controller/admin/account.controller.js
+++ controller/admin/account.controller.js
@@ -56,3 +56,45 @@ module.exports.createPost = async (req, res) => {
     res.redirect(`${systemConfig.prefixAdmin}/accounts`);
   }
 };
+// [get] /admin/account/edit/:id
+
+module.exports.edit = async (req, res) => {
+  let find = {
+    _id: req.params.id,
+    deleted: false,
+  };
+  try {
+    const data = await Account.findOne(find);
+    const roles = await Role.find({
+      deleted: false,
+    });
+    res.render("admin/pages/accounts/edit", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      data: data,
+      roles: roles,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+  }
+};
+//[patch] /admin/account/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  const emailExit = await Account.findOne({
+    _id: { $ne: id },
+    email: req.body.email,
+    deleted: false,
+  });
+  if (emailExit) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại!`);
+  } else {
+    if (req.body.password) {
+      req.body.password = md5(req.body.password);
+    } else {
+      delete req.body.password;
+    }
+    await Account.updateOne({ _id: id }, req.body);
+    req.flash("success", "Cập nhật tài khoản thành công");
+  }
+  res.redirect("back");
+};
--- routers/admin/account.router.js
+++ routers/admin/account.router.js
@@ -14,4 +14,13 @@ router.post(
   validates.createPost,
   controller.createPost
 );
+router.get("/edit/:id", controller.edit);
+router.patch(
+  "/edit/:id",
+  upload.single("avatar"),
+  uploadCloud.upload,
+  validates.editPatch,
+  controller.editPatch
+);
+
 module.exports = router;

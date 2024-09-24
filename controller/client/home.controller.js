450305967e0275fc9a8c490e7f5d9e5338819b86
kien <dp1.1a2kien@gmail.com>
2024-09-24 23:25:09 +0700

    hiển thị danh sách nổi bật

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -1,6 +1,18 @@
-// [get] /
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/product");
+// [get]
 module.exports.index = async (req, res) => {
+  //lấy ra sản phẩm nổi bật
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  });
+  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+
+  // hết lấy ra sản phẩm nổi bật
   res.render("client/pages/home/index.pug", {
     pageTitle: "Trang gốc",
+    productsFeatured: newProducts,
   });
 };
--- controller/client/products.controller.js
+++ controller/client/products.controller.js
@@ -1,18 +1,12 @@
 const Product = require("../../models/product.model.js");
-
+const productsHelper = require("../../helpers/product");
 // [get] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
     deleted: false,
   }).sort({ position: "desc" });
-  const newProducts = products.map((item) => {
-    item.priceNew = (
-      (item.price * (100 - item.discountPercentage)) /
-      100
-    ).toFixed(0);
-    return item;
-  });
+  const newProducts = productsHelper.priceNewProducts(products);
 
   res.render("client/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",

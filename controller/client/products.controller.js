c9315ba94d1ab1cdb027ba1acfeedad56558e367
kien <dp1.1a2kien@gmail.com>
2024-09-25 22:20:05 +0700

    trang chi tiết sản phẩm bên client

--- controller/client/products.controller.js
+++ controller/client/products.controller.js
@@ -15,16 +15,25 @@ module.exports.index = async (req, res) => {
     products: newProducts,
   });
 };
-// [get] /products/:slug
+// [get] /products/:slugProduct
 module.exports.detail = async (req, res) => {
   try {
     const find = {
       deleted: false,
-      //slug: req.params.slug,
-      _id: req.params.id,
+      slug: req.params.slugProduct,
+
+      status: "active",
     };
     const product = await Product.findOne(find);
-    console.log(product);
+    if (product.product_category_id) {
+      const category = await ProductCategory.findOne({
+        _id: product.product_category_id,
+        status: "active",
+        deleted: false,
+      });
+      product.category = category;
+    }
+    product.priceNew = productsHelper.priceNewProduct(product);
     res.render("client/pages/products/detail", {
       pageTitle: product.title,
       product: product,
--- routers/client/products.router.js
+++ routers/client/products.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require("../../controller/client/products.controller");
 router.get("/", controller.index);
 router.get("/:slugCategory", controller.category);
+router.get("/detail/:slugProduct", controller.detail);
 // router.get("/:slug", controller.detail);
 // router.get("/:id", controller.detail);
 module.exports = router;

4a3ea25e528c645094f8d82a1c4f516fef6346ca
kien <dp1.1a2kien@gmail.com>
2024-09-25 13:17:37 +0700

    danh sách sản phẩm bên client

--- controller/client/products.controller.js
+++ controller/client/products.controller.js
@@ -1,4 +1,6 @@
 const Product = require("../../models/product.model.js");
+const ProductCategory = require("../../models/product-category.model.js");
+const ProductCategoryHelper = require("../../helpers/product-category.js");
 const productsHelper = require("../../helpers/product");
 // [get] /products
 module.exports.index = async (req, res) => {
@@ -30,4 +32,26 @@ module.exports.detail = async (req, res) => {
   } catch (error) {
     res.redirect(`/products`);
   }
+};
+// [get] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    deleted: false,
+  });
+
+  const lisSubCategory = await ProductCategoryHelper.getSubCategory(
+    category.id
+  );
+  const lisSubCategoryId = lisSubCategory.map((item) => item.id);
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...lisSubCategoryId] },
+    deleted: false,
+  }).sort({ position: "desc" });
+
+  const newProductsNew = productsHelper.priceNewProducts(products);
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: newProductsNew,
+  });
 };

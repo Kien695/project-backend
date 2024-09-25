36f465346db43f4512fbf3936cbead810920c479
kien <dp1.1a2kien@gmail.com>
2024-09-25 11:21:00 +0700

    Hiển thị danh sách sản phẩm mới

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -11,8 +11,16 @@ module.exports.index = async (req, res) => {
   const newProducts = productsHelper.priceNewProducts(productsFeatured);
 
   // hết lấy ra sản phẩm nổi bật
+  //lấy ra sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  }).sort({ position: "desc" });
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
+  //hết lấy ra sản phẩm mới nhất
   res.render("client/pages/home/index.pug", {
     pageTitle: "Trang gốc",
     productsFeatured: newProducts,
+    productsNew: newProductsNew,
   });
 };

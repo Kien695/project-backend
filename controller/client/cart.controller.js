233bf4eb09ca4f050aaee97ee71c514d9389c8c1
kien <dp1.1a2kien@gmail.com>
2024-09-27 23:01:41 +0700

    làm trang giỏ hàng

--- controller/client/cart.controller.js
+++ controller/client/cart.controller.js
@@ -1,4 +1,33 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/product");
+//[get]/cart/
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const cart = await Cart.findOne({
+    _id: cartId,
+  });
+  if (cart.products.length > 0) {
+    for await (const item of cart.products) {
+      const productId = item.product_id;
+      const productInfo = await Product.findOne({
+        _id: productId,
+        deleted: false,
+      }).select("title thumbnail slug price discountPercentage");
+      productInfo.priceNew = productsHelper.priceNewProduct(productInfo);
+      item.productInfo = productInfo;
+      item.totalPrice = productInfo.priceNew * item.quantity;
+    }
+  }
+  cart.totalPrice = cart.products.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0
+  );
+  res.render("client/pages/carts/index.pug", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart,
+  });
+};
 //[post]/cart/add/:productId
 module.exports.addPost = async (req, res) => {
   const productId = req.params.productId;

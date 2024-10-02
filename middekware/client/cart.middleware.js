fd9b1bc567bea207a58b1de5fe5920aed87997ac
kien <dp1.1a2kien@gmail.com>
2024-10-02 21:38:54 +0700

    sửa trang giỏ hàng 1 xí

--- middekware/client/cart.middleware.js
+++ middekware/client/cart.middleware.js
@@ -1,23 +1,35 @@
 const Cart = require("../../models/cart.model");
+
 module.exports.cartId = async (req, res, next) => {
   if (!req.cookies.cartId) {
-    //tạo giỏ hàng
+    // Tạo giỏ hàng mới
     const cart = new Cart();
     await cart.save();
-    const expiresCookie = 5 * 24 * 60 * 60 * 1000;
+    const expiresCookie = 5 * 24 * 60 * 60 * 1000; // 5 ngày
     res.cookie("cartId", cart.id, {
       expires: new Date(Date.now() + expiresCookie),
     });
   } else {
-    //lấy ra
+    // Lấy giỏ hàng hiện có
     const cart = await Cart.findOne({
       _id: req.cookies.cartId,
     });
-    cart.totalQuantity = cart.products.reduce(
-      (sum, item) => sum + item.quantity,
-      0
-    );
-    res.locals.miniCart = cart;
+
+    if (cart) {
+      // Kiểm tra nếu giỏ hàng trống
+      if (cart.products.length === 0) {
+        console.log("Giỏ hàng hiện đang trống.");
+      } else {
+        // Tính tổng số lượng sản phẩm
+        cart.totalQuantity = cart.products.reduce(
+          (sum, item) => sum + item.quantity,
+          0
+        );
+        res.locals.miniCart = cart;
+      }
+    } else {
+      console.log("Không tìm thấy giỏ hàng.");
+    }
   }
   next();
 };
